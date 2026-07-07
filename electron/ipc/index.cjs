6cfa438a1819795e7afaf2ccb07ecfa4ec602480
HADES <32095797+HDAES@users.noreply.github.com>
2026-07-08 01:50:11 +0800

    refactor: centralize ipc channel names

--- electron/ipc/index.cjs
+++ electron/ipc/index.cjs
@@ -1,5 +1,10 @@
+const IPC_CHANNELS = Object.freeze({
+  PING: 'ping'
+});
+
 function registerIpcHandlers(ipcMain) {
-  ipcMain.handle('ping', () => 'pong');
+  ipcMain.removeHandler(IPC_CHANNELS.PING);
+  ipcMain.handle(IPC_CHANNELS.PING, () => 'pong');
 }
 
-module.exports = { registerIpcHandlers };
+module.exports = { registerIpcHandlers, IPC_CHANNELS };
